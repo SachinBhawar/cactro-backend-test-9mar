9881f45c43d46bc38cfaa51f50a6eafeca5472c0
Sachin Bhawar <95158797+SachinBhawar@users.noreply.github.com>
2025-03-09 14:43:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import app from "./index.js";
 
-app.listen(3600, (req, res) => {
+const PORT = process.env.PORT || 3600;
+app.listen(PORT, (req, res) => {
     console.log("Server is listening on 3600.");
 });
