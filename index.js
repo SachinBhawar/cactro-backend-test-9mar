70880513fc1d89a04b1f3030aef7118d8b6fea19
Sachin Bhawar <95158797+SachinBhawar@users.noreply.github.com>
2025-03-11 13:43:13 +0530

    Update cors policy

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // import modules and liabraries
 import express, { json } from "express";
 import cookieParcer from "cookie-parser";
-// import swagger from "swagger-ui-express";
+import swagger from "swagger-ui-express";
 // import apiDocs from "./swagger.json" assert { type: "json" };
+import apiDocs from "./swagger.json" with { type: "json" };
+
 
 import {
     createIssueController,
@@ -10,6 +12,7 @@ import {
     getUserDetailsController,
 } from "./src/Controller/controller.js";
 import { errorHandlerMiddleware } from "./src/Middlewares/ApplicationError.js";
+import cors from 'cors'
 
 // rest api
 const app = express();
@@ -17,6 +20,14 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(cookieParcer());
+app.use(cors());
+app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
 
 app.get("/github", getUserDetailsController);
 app.get("/github/:repoName", getRepoDetailsController);
@@ -25,12 +36,12 @@ app.post("/github/:repoName/issues", createIssueController);
 app.use(errorHandlerMiddleware);
 
 // Swagger documentation
-// app.use("/api-docs", swagger.serve, swagger.setup(apiDocs));
+app.use("/api-docs", swagger.serve, swagger.setup(apiDocs));
 
 app.all("*", (req, res) => {
     res.status(404).send({
         success: false,
-        msg: "This route is not Supported by this app.",
+        msg: "This route is not Supported by this app. For referece please check documentation at /api-docs",
     });
 });
 
